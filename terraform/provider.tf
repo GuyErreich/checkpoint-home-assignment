ed89ce57086e233de6a0ec7ed70f5679db14880b
GuyErreich <gerreich.dev@gmail.com>
2025-06-10 01:44:10 +0300

    added backend support for terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,14 @@
 terraform {
+  required_version = ">= 1.0"
+  
+  backend "s3" {
+    bucket       = "terraform-state-backend-shared"
+    key          = "checkpoint-assignment/ecs-microservices/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
